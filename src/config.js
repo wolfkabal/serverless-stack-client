1b85d469fe610759a2d0b98df3e7a24c4c11fe29
wolfkabal <wolfkabal@gmail.com>
2020-01-21 09:56:32 -0500

    Setting up our React app

--- src/config.js
+++ src/config.js
@@ -1,16 +1,16 @@
 export default {
   s3: {
     REGION: "us-east-1",
-    BUCKET: ""
+    BUCKET: "serverless-notes-app-upload"
   },
   apiGateway: {
     REGION: "us-east-1",
-    URL: ""
+    URL: "https://yitce62o4f.execute-api.us-east-1.amazonaws.com/prod"
   },
   cognito: {
     REGION: "us-east-1",
-    USER_POOL_ID: "",
-    APP_CLIENT_ID: "",
-    IDENTITY_POOL_ID: ""
+    USER_POOL_ID: "us-east-1_GMLxqJTBt",
+    APP_CLIENT_ID: "50t5a5f00r2c9b0ceoa1bsi7r",
+    IDENTITY_POOL_ID: "us-east-1:71031219-dfc1-4c50-a34d-22350900dfb7"
   }
 };
