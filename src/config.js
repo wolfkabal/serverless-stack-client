9bb8b860f9f510d6286035a93de9854df3ce63dd
wolfkabal <wolfkabal@gmail.com>
2020-01-20 22:38:38 -0500

    Created the base react front-end

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: ""
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: ""
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "",
+    APP_CLIENT_ID: "",
+    IDENTITY_POOL_ID: ""
+  }
+};
