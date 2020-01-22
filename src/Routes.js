9b2a7ef6dcc68ca3939939216d7cc3ccd579a22b
wolfkabal <wolfkabal@gmail.com>
2020-01-21 20:40:28 -0500

    Front-end enhancements login, create user

--- src/Routes.js
+++ src/Routes.js
@@ -2,11 +2,16 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
+import Login from "./containers/Login";
+import AppliedRoute from "./components/AppliedRoute";
+import Signup from "./containers/Signup";
 
-export default function Routes() {
+export default function Routes({ appProps }) {
     return(
         <Switch>
-            <Route path="/" exact component={Home} />
+            <Route path="/" exact component={Home} appProps={appProps} />
+            <Route path="/login" exact component={Login} appProps={appProps} />
+            <Route path="/signup" exact component={Signup} appProps={appProps} />
             { /* Finally, catch all unmatched routes */ }
             <Route component={NotFound} />
         </Switch>
